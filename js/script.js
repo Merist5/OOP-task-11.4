47ff8a49d2aa3c6f7e11110cf9a793416b76fa4e
Joanna Urbiak <joanna.urbiak@o2.pl>
2018-05-29 22:10:05 +0200

    Add files & catalogues

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function Phone(brand, price, color){
+  this.brand = brand;
+  this.price = price;
+  this.color =color;
+}
+
+Phone.prototype.printInfo = function() {
+  console.log("The phone brand is " + this.brand + ", color is " + this.color 
+  + " and the price is " + this.price + ".");
+}
+
+var SmsngGalaxyS6 = new Phone("Samsung", 2000, "black");
+var iPhone6s = new Phone("Apple", 3000, "rose gold");
+var OneplusOne = new Phone("OnePlus", 1000, "silver");
+
+SmsngGalaxyS6.printInfo();
+iPhone6s.printInfo();
+OneplusOne.printInfo();
